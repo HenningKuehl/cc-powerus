6ec21819bf31d8dd390bfe8c9ce219bf524b864c
Henning Kuehl <henning@kuehlonline.de>
2022-12-06 21:23:18 +1300

    define response model

--- src/flights/controllers/flights.controller.ts
+++ src/flights/controllers/flights.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from "@nestjs/common";
 import { FlightsService } from '../services/flights.service';
+import { ApiResponse } from "../../shared/interfaces/api-response.interface";
+import { Flight } from "../interfaces/flight.interface";
 
 @Controller('flights')
 export class FlightsController {
   constructor(private readonly flightsService: FlightsService) {}
 
   @Get()
-  async getAll() {
-    const flights = await this.flightsService.getAllFlights();
-    return {
-      success: true,
-      statusCode: 200,
-      data: flights,
-      message: 'Ok'
-    };
+  async getAll(): Promise<ApiResponse<Flight[] | null>> {
+    try {
+      const flights = await this.flightsService.getAllFlights();
+      return {
+        success: true,
+        statusCode: HttpStatus.OK,
+        data: flights,
+        message: 'Ok',
+      };
+    } catch (e) {
+      console.error('Error while getting all flights', e);
+      return {
+        data: null,
+        message: 'Internal server error',
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        success: false,
+      }
+    }
   }
 }
---
+++ src/shared/interfaces/api-response.interface.ts
@@ -0,0 +1,8 @@
+import { HttpStatus } from "@nestjs/common";
+
+export interface ApiResponse<T> {
+  success: boolean;
+  statusCode: HttpStatus;
+  message: string;
+  data: T;
+}
