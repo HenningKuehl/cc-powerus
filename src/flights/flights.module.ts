4ab28a37540037675b47bb028f9e0e3f6f7c4677
Henning Kuehl <henning@kuehlonline.de>
2022-12-06 21:24:37 +1300

    add caching to flight sources

--- src/flights/flights.module.ts
+++ src/flights/flights.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from "@nestjs/common";
 import { FlightsController } from './controllers/flights.controller';
 import { FlightsService } from './services/flights.service';
 import { FlightsRepository } from './repositories/flights.repository';
-import { HttpModule } from "@nestjs/axios";
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register()],
   controllers: [FlightsController],
   providers: [FlightsService, FlightsRepository],
 })
--- src/flights/repositories/flights.repository.ts
+++ src/flights/repositories/flights.repository.ts
@@ -1,30 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Flight } from '../interfaces/flight.interface';
 import { FlightResponse } from '../interfaces/flight-response.interface';
-import { catchError, firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom, map, tap } from 'rxjs';
+import { Cache } from 'cache-manager';
 
 const SERVER_BASE_URL = 'https://coding-challenge.powerus.de/flight';
 
 @Injectable()
 export class FlightsRepository {
-  constructor(private readonly http: HttpService) {}
+  constructor(
+    private readonly http: HttpService,
+    @Inject(CACHE_MANAGER) private cache: Cache,
+  ) {}
 
   async getFlightsBySource(sourceId: string): Promise<Flight[]> {
+    const cacheKey = `flights_${sourceId}`;
+    const cachedFlights = await this.cache.get<Flight[]>(cacheKey);
+    if (cachedFlights) {
+      console.log('return cached flights');
+      return cachedFlights;
+    }
+
     try {
-      // TODO: play with timeout config
-      const resp = await firstValueFrom(
+      return await firstValueFrom(
         this.http
           .get<FlightResponse>(`${SERVER_BASE_URL}/${sourceId}`, {
             timeout: 3000,
           })
           .pipe(
+            map((resp) => resp.data.flights),
+            tap((flights) => {
+              // Cached data are valid for one hour
+              this.cache.set(cacheKey, flights, 60 * 60 * 1000);
+            }),
             catchError((e) => {
               throw e;
             }),
           ),
       );
-      return resp.data.flights;
     } catch (e) {
       // TODO: think about adding firebase functions logger
       console.error('Error while fetching flights from source1', e);
