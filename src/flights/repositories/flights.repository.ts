8dd1ebb66aa80837cae3e9f3d45106b47f967032
Henning Kuehl <henning@kuehlonline.de>
2022-12-07 08:51:19 +1300

    cleaner error handling

--- src/flights/repositories/flights.repository.ts
+++ src/flights/repositories/flights.repository.ts
@@ -1,8 +1,8 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { Flight } from '../interfaces/flight.interface';
+import { IFlight } from '../interfaces/flight.interface';
 import { FlightResponse } from '../interfaces/flight-response.interface';
-import { catchError, firstValueFrom, map, tap } from 'rxjs';
+import { catchError, firstValueFrom, map, of, retry, tap } from 'rxjs';
 import { Cache } from 'cache-manager';
 
 const SERVER_BASE_URL = 'https://coding-challenge.powerus.de/flight';
@@ -11,38 +11,35 @@ const SERVER_BASE_URL = 'https://coding-challenge.powerus.de/flight';
 export class FlightsRepository {
   constructor(
     private readonly http: HttpService,
-    @Inject(CACHE_MANAGER) private cache: Cache,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
   ) {}
 
-  async getFlightsBySource(sourceId: string): Promise<Flight[]> {
+  async getFlightsBySource(sourceId: string): Promise<IFlight[]> {
     const cacheKey = `flights_${sourceId}`;
-    const cachedFlights = await this.cache.get<Flight[]>(cacheKey);
+    const cachedFlights = await this.cache.get<IFlight[]>(cacheKey);
     if (cachedFlights) {
       console.log('return cached flights');
       return cachedFlights;
     }
 
-    try {
-      return await firstValueFrom(
-        this.http
-          .get<FlightResponse>(`${SERVER_BASE_URL}/${sourceId}`, {
-            timeout: 3000,
-          })
-          .pipe(
-            map((resp) => resp.data.flights),
-            tap((flights) => {
-              // Cached data are valid for one hour
-              this.cache.set(cacheKey, flights, 60 * 60 * 1000);
-            }),
-            catchError((e) => {
-              throw e;
-            }),
-          ),
-      );
-    } catch (e) {
-      // TODO: think about adding firebase functions logger
-      console.error('Error while fetching flights from source1', e);
-      return [];
-    }
+    return await firstValueFrom(
+      this.http
+        .get<FlightResponse>(`${SERVER_BASE_URL}/${sourceId}`, {
+          timeout: 3000,
+        })
+        .pipe(
+          map((resp) => resp.data.flights),
+          tap((flights) => {
+            // Cached data are valid for one hour
+            this.cache.set(cacheKey, flights, 60 * 60 * 1000);
+          }),
+          catchError((e) => {
+            // TODO: think about adding firebase functions logger
+            console.error('Error while fetching flights from source1', e);
+            return of([]);
+          }),
+          retry(3),
+        ),
+    );
   }
 }
